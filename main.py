4ecba127854a942807dc35989aa2a24e00e1d9e9
markunasn <markunasn@gmail.com>
2024-08-22 22:18:03 -0500

    added char counter function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     book_path = "books/frankenstein.txt"
     text = read_book(book_path)
     print(word_count(text))
+    print(char_count(text))
       
 
 def read_book(book):
@@ -11,4 +12,15 @@ def read_book(book):
 def word_count(book):
     words = book.split()
     return len(words)
+
+def char_count(book):
+    counts = {}
+    text = book.lower()
+    for char in text:
+        if char in counts:
+            counts[char] += 1
+        else:
+            counts[char] = 1
+    return counts
+
 main()
